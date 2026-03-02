967303273fb7ae7a33f22e1ac00a7f3c177185d5
Danny Morabito <danny@arx-ccn.com>
2026-03-02 09:09:39 -0300

    fix staging test

--- test/utils/app_flavor_test.dart
+++ test/utils/app_flavor_test.dart
@@ -2,9 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:whitenoise/utils/app_flavor.dart';
 
 void main() {
+   const appFlavor = String.fromEnvironment('APP_FLAVOR');
+
   group('isStaging', () {
-    test('is false when APP_FLAVOR is not set', () {
-      expect(isStaging, isFalse);
+    test('matches APP_FLAVOR', () {
+      expect(isStaging, appFlavor == 'staging');
     });
 
     test('is a bool', () {
