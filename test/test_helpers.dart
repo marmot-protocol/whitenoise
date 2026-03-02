952e52aeb199972135ce188e0bcfce9c12081e26
Danny Morabito <danny@arx-ccn.com>
2026-03-02 13:12:36 -0300

    fix failing test

--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-import 'dart:typed_data';
 
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -8,7 +8,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart' show ScreenUtilInit;
 import 'package:flutter_test/flutter_test.dart'
     show TestWidgetsFlutterBinding, WidgetTester, addTearDown;
-import 'package:flutter/services.dart';
 import 'package:whitenoise/l10n/generated/app_localizations.dart';
 import 'package:whitenoise/routes.dart';
 
