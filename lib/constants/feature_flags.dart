af83d12bdb7c372b7491cda20d3a0c6d85ac0f04
josefinalliende <mariajosefinaalliende@gmail.com>
2026-05-18 14:43:42 -0400

    chore: remove feature flag provider

--- lib/constants/feature_flags.dart
+++
@@ -1,7 +0,0 @@
-enum FeatureFlag { leaveGroup }
-
-const _featureFlagValues = {
-  FeatureFlag.leaveGroup: bool.fromEnvironment('LEAVE_GROUP_ENABLED'),
-};
-
-bool featureFlagValue(FeatureFlag flag) => _featureFlagValues[flag] ?? false;
