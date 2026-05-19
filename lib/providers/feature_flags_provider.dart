dba52c10dccfdefa36c375422df4ca2a9e5af917
Pepi <mariajosefinaalliende@gmail.com>
2026-05-19 10:17:33 -0400

    Enable leave group (#675)
    * chore: remove feature flag from use leave group hook
    
    * chore: remove feature flag from chat list tile
    
    * chore: remove feature flag provider
    
    * docs: update changelog

--- lib/providers/feature_flags_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:whitenoise/constants/feature_flags.dart';
-
-final featureFlagProvider = Provider.family<bool, FeatureFlag>(
-  (ref, flag) => featureFlagValue(flag),
-);
